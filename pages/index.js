80ddfc4ffa5de4afee884936b12c5e201f1157bc
Paaras <paaras@campus.uni-paderborn.de>
2020-05-28 22:51:48 +0530

    added entity filter

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import React from 'react';
-import {view} from 'react-easy-state';
+import React, { useState } from 'react';
+import {view, store} from 'react-easy-state';
 import Layout from '../components/Layout';
 import Spinner from '../components/Spinner';
 import searchStore from '../stores/search';
@@ -29,10 +29,39 @@ const wrap = body => (
   </Layout>
 );
 
+
+const count = store({
+	state: 0
+});
+
+const  refinedResults = [];
+
 export default view(() => {
-  const {status, results} = searchStore;
+	
+//	const [filter, setFilter] = useState(false);
+	const {status, results} = searchStore;
+	//refinedResults = results;
+	
+	
+	const counter = store({
+		
+		  increment: (event) => {
+			  console.log("dropvalue "+ event.target.value)
+			  refinedResults.length =0
+			   for (var i = 0; i < results.length; i++) {
+				   if(event.target.value==results[i].title)
+					     refinedResults.push(results[i]);
+				   }
+			  
+			  count.state = count.state + 1 ;
+			  console.log("count.state "+count.state)
+				   console.log("refinedResults "+ refinedResults)
+				   //this.forceUpdate()
+				   }
+		});
 
   if (status === 'loading') {
+	  count.state=0;
     return wrap(
       <div className="row centerContent">
         <Spinner />
@@ -49,7 +78,11 @@ export default view(() => {
     );
   }
 
-  if (results && results.length) {
+  
+  
+  if (results && results.length && !count.state) {
+	  console.log("count.state "+count.state);
+	  console.log("results "+refinedResults)
     return wrap(
       <React.Fragment>
         <style jsx>{`
@@ -57,11 +90,56 @@ export default view(() => {
             padding-bottom: 30px;
           }
         `}</style>
+        <select value={results.title} onChange = {(e)=>counter.increment(e)}>
+        {
+        	results.map((team) => <option key={team.url} value={team.title} >{team.title}</option>)
+        }
+        </select>
+        
+        {
+        	
+        	results.map(it => (
+          <div className="row paddedRow" key={it.url}>
+            <div className="col-sm-2">
+              <img className="img-fluid rounded" src={it.image} alt={it.image} />
+            </div>
+            <div className="col-sm-10">
+              <h4>{it.title}</h4>
+              <p>{it.description}</p>
+              <Link
+                href={{
+                  pathname: '/resource',
+                  query: {resource: it.url, title: it.title},
+                }}>
+                <a>Show full details</a>
+              </Link>
+            </div>
+          </div> 
+        ))}
 
-         <select value={results.title}>
-        {results.map((team) => <option key={team.url} value={team.title}>{team.title}</option>)}
-      </select>
-        {results.map(it => (
+      </React.Fragment>
+    );
+  }
+  
+if (count.state) {
+	  
+	  console.log("filterTrue "+refinedResults )
+    return wrap(
+      <React.Fragment>
+        <style jsx>{`
+          .paddedRow {
+            padding-bottom: 30px;
+          }
+        `}</style>
+        <select value={results.title} onChange = {(e)=>counter.increment(e)}>
+        {
+        	results.map((team) => <option key={team.url} value={team.title} >{team.title}</option>)
+        }
+        </select>
+        
+        {
+        	
+        	refinedResults.map(it => (
           <div className="row paddedRow" key={it.url}>
             <div className="col-sm-2">
               <img className="img-fluid rounded" src={it.image} alt={it.image} />
